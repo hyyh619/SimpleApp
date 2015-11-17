6c9a93fc023d9a70bad5f1a76f598d7f62d10a56
Huang Ying <hyyh619@hotmail.com>
2015-11-17 15:15:19 +0800

    SimpleApp: just a test for github commit.
    By: ying.huang.

--- app/src/main/java/com/hy/simpleapp/MainActivity.java
+++ app/src/main/java/com/hy/simpleapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // just for github test.
+        // just for github test. once again
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
